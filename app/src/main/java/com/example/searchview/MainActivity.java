4a55fe13435c15f7956aed42399aa9eaf4730a4b
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-10-17 11:33:07 +0600

    assigned

--- app/src/main/java/com/example/searchview/MainActivity.java
+++ app/src/main/java/com/example/searchview/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.searchview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayAdapter adapter;
+    ArrayList<String>list1;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView=findViewById(R.id.list);
+        searchView=findViewById(R.id.search);
+        list1.add("Monday");
+        adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,list1);
     }
 }
