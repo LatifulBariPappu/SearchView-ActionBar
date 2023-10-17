1773e62e7b5104f4eda579eb44e02d1ccf0d041d
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2023-10-17 11:40:11 +0600

    search view query

--- app/src/main/java/com/example/searchview/MainActivity.java
+++ app/src/main/java/com/example/searchview/MainActivity.java
@@ -22,6 +22,25 @@ public class MainActivity extends AppCompatActivity {
         listView=findViewById(R.id.list);
         searchView=findViewById(R.id.search);
         list1.add("Monday");
+        list1.add("Tuesday");
+        list1.add("Wednesday");
+        list1.add("Thursday");
+        list1.add("Friday");
+        list1.add("Saturday");
+        list1.add("Sunday");
+
         adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,list1);
+        listView.setAdapter(adapter);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
